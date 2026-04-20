81916e25df93c80830fcdc8d4428c142c89bc9bd
P4tr1ck191 <283221@vutbr.cz>
2026-04-20 13:40:47 +0200

    treti

---
+++ student_grades.py
@@ -0,0 +1,36 @@
+class StudentsGrades:
+    def __init__(self, scores):
+        self.scores = scores
+
+    def get_by_index(self, index):
+        return self.scores[index]
+
+    def count(self):
+        return len(self.scores)
+
+    def get_grade(self, index):
+        score = self.scores[index]
+        if score >= 90:
+            return "A"
+        elif score >= 80:
+            return "B"
+        elif score >= 70:
+            return "C"
+        elif score >= 60:
+            return "D"
+        elif score >= 50:
+            return "E"
+        else:
+            return "F"
+
+
+results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
+
+print(results.count())          # 9
+print(results.get_by_index(2))  # 91
+print(results.scores)           # [85, 42, 91, 67, 50, 73, 100, 38, 58]
+print(results.get_grade(2))  # A (91 bodů)
+print(results.get_grade(6))  # A (100 bodů)
+print(results.get_grade(7))  # F (38 bodů)
+
+
