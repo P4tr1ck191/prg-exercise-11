4f5da0790b19aa0a87ce49663b51e4282716a366
P4tr1ck191 <283221@vutbr.cz>
2026-04-20 14:00:15 +0200

    paty

--- student_grades.py
+++ student_grades.py
@@ -23,8 +23,24 @@ class StudentsGrades:
         else:
             return "F"
 
-    def find(self):
+    def find(self, value):
+        result = []
+        i = 0
+        for score in self.scores:
+            if score == value:
+                result.append(i)
+            i += 1
+        return result
 
+    def get_sorted(self):
+        scores = self.scores[:]
+
+        for serazeno_od_konce in range(len(scores)):
+             for pozice_porovnani in range(len(scores) - 1 - serazeno_od_konce):
+                 if scores[pozice_porovnani] > scores[pozice_porovnani + 1]:
+                         scores[pozice_porovnani], scores[pozice_porovnani + 1] = scores[pozice_porovnani + 1], scores[pozice_porovnani]
+
+        return scores
 
 results = StudentsGrades([85, 42, 91, 67, 50, 73, 100, 38, 58])
 
@@ -34,5 +50,8 @@ print(results.scores)           # [85, 42, 91, 67, 50, 73, 100, 38, 58]
 print(results.get_grade(2))  # A (91 bodů)
 print(results.get_grade(6))  # A (100 bodů)
 print(results.get_grade(7))  # F (38 bodů)
-
-
+print(results.find(100))  # [6]
+print(results.find(50))   # [4]
+print(results.find(77))   # []
+print(results.get_sorted())   # [38, 42, 50, 58, 67, 73, 85, 91, 100]
+print(results.scores)
